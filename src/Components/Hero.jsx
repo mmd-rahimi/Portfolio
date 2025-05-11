6272f8e7366aa6016b24f2e9d4994002bceb2c5f
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-05-11 14:43:11 -0700

    styling wellll

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,9 +6,9 @@ function Hero() {
   return (
     <div
       className="hero text-[#EEEEEE]
-      w-full h-[45rem] py-16 px-6 md:h-[37rem] md:px-12"
+      w-full h-[45rem] py-22 px-6 md:h-[37rem] md:px-12"
     >
-      <div className="flex flex-col gap-7">
+      <div className="flex flex-col gap-5">
         <span className="font-semibold text-5xl md:text-[5rem] ">Hello <span className="text-[#00ADB5]">.</span></span> <br />
         <h2 className="text-5xl font-bold text-center md:text-[5rem] md:text-left">
           <span className="font-bold text-6xl text-[#00ADB5]">__ </span>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { IoMenuSharp } from "react-icons/io5";
 import Hmenu from "./Hmenu";
+import "../styles/text.css";
 
 function Navbar() {
   return (
-    <div className="flex flex-row justify-between py-4 px-6 text-[#EEEEEE] bg-[#222831] items-center md:py-8 md:px-8">
-      <div className="font-bold text-[.9rem] md:text-xl">Mohamad Rahimi</div>
-      <div className="">
-      {/* <IoMenuSharp size={23}/> */}
-      <Hmenu />
+    <div className="fixed w-full">
+      <div className=" flex flex-row justify-between py-4 px-6 text-[#EEEEEE] bg-[#222831] items-center md:py-8 md:px-8">
+        <div className="text-[#00ADB5] font-bold text-[.9rem] md:text-xl">Mohamad Rahimi</div>
+        <div className="">
+          {/* <IoMenuSharp size={23}/> */}
+          <Hmenu />
+        </div>
       </div>
     </div>
   );
