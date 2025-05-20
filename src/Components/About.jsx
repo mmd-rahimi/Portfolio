ea2db650f66916be0b6ba3a8fdd668acfcc3822b
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-05-20 15:20:56 -0700

    desining

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,18 +4,23 @@ import { FaCode } from 'react-icons/fa'
 
 function About() {
   return (
-    <div className='px-8 flex flex-col gap-14 text-[#EEEEEE] bg-[#222831]'>
-        <div className='text-lg font-[600] flex flex-col gap-5'>
+    <div className='px-8 flex flex-col gap-14 text-[#EEEEEE] bg-[#222831] md:grid grid-cols-2'>
+        <div className='text-lg font-[600] flex flex-col justify-center gap-8 md:text-2xl'>
             <div className='flex flex-row gap-8 items-center'><div><FaCode className='size-7 text-[#00ADB5]'/></div><div>Website Development</div></div>
-            <div className='flex flex-row gap-8 items-center'><div><FaCode className='size-7 text-[#00ADB5]'/></div><div>Website Development</div></div>
+            <div className='flex flex-row gap-8 items-center'><div><FaCode className='size-7 text-[#00ADB5]'/></div><div>App Development</div></div>
             <div className='flex flex-row gap-8 items-center'><div><FaCode className='size-7 text-[#00ADB5]'/></div><div>Website Development</div></div>
             <div className='flex flex-row gap-8 items-center'><div><FaCode className='size-7 text-[#00ADB5]'/></div><div>Website Development</div></div>
         </div>
         <div className='flex flex-col gap-7'>
             <div className='text-4xl font-bold'>About <span className='text-[#00ADB5]'>me</span></div>
-            <p className='text-lg'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime tenetur aperiam molestiae inventore, sequi natus, porro sed dolores quaerat atque, expedita adipisci voluptate commodi reprehenderit repudiandae amet exercitationem minus nostrum?
-                Alias beatae iure ex doloribus quod dolorem saepe sequi impedit consequuntur inventore est et voluptate dicta eius ducimus quas at, voluptatum quo quisquam ad illo. Totam dignissimos mollitia qui quasi?. Accusantium soluta temporibus natus, deleniti pariatur corporis harum hic in reprehenderit nemo assumenda fuga neque tempora quasi laborum illo eius asperiores earum voluptatem.
+            <p className='text-lg md:text-xl'>
+            I`m a patient and very flexible person who loves progress.
+            <br /> I enjoy codding and good at teamwork.
+            <br />
+            I am a frontend developer with a strong passion for building interactive and performant web applications using modern tools like
+             <span className='text-[#00ADB5] font-semibold'> React </span>,<span className='text-[#00ADB5] font-semibold'>Next.js </span>,<span className='text-[#00ADB5] font-semibold'>Tailwind CSS</span> , and more.
+             I started learning programming at a bootcamp<span className='font-light text-sm md:text-md text-[#00ADB5]'>(for 8 months) </span>and continued it as self-study.
+             I have 6 months of work experience at<span className='text-[#00ADB5]'> AFTAB-NEGAR </span>Company as a admin platform & 6 months of work experience at<span className='text-[#00ADB5]'> MOODIC Studio </span>as a admin platform.
             </p>
         </div>
 
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,14 +11,14 @@ function Contact() {
       <div className='flex justify-start items-center'>
       <div className='text-4xl font-bold'>Contact <span className='text-[#00ADB5]'>me</span></div>
       </div>
-<div className='flex flex-col gap-10'>
+<div className='flex flex-col gap-10  md:grid grid-cols-2'>
 <div className='contactBox px-8 py-7 flex flex-col gap-8 justify-center items-start rounded-2xl shadow-md shadow-[#00ADB5]'>
         <div className='flex justify-start items-center'>
           <div className='flex flex-row justify-start items-center gap-4'>
-            <div className='text-'><FaPhone className='size-7 text-[#00ADB5]'/></div>
+            <div className='text-'><FaPhone className='size-7 sm:size-8 text-[#00ADB5]'/></div>
             <div className='flex flex-col justify-start items-start'>
-              <p className='font-light text-sm text-gray-400'>Phone</p>
-              <p className='text-md'>+98 930 6262 923</p>
+              <p className='font-light text-sm sm:text-md text-gray-400'>Phone</p>
+              <p className='text-md sm:text-lg'>+98 930 6262 923</p>
             </div>
           </div>
         </div>
@@ -27,10 +27,10 @@ function Contact() {
       <div className='contactBox px-8 py-7 flex flex-col gap-8 justify-center items-start rounded-2xl shadow-md shadow-[#00ADB5]'>
         <div className='flex justify-start items-center'>
           <div className='flex flex-row justify-start items-center gap-4'>
-            <div className='text-'><MdEmail className='size-8 text-[#00ADB5]'/></div>
+            <div className='text-'><MdEmail className='size-8 sm:size-9 text-[#00ADB5]'/></div>
             <div className='flex flex-col justify-start items-start'>
-              <p className='font-light text-sm text-gray-400'>Email</p>
-              <p className='text-md'>mohamad8rahimi3@gmail.com</p>
+              <p className='font-light text-sm sm:text-md text-gray-400'>Email</p>
+              <p className='text-md sm:text-lg'>mohamad8rahimi3@gmail.com</p>
             </div>
           </div>
         </div>
@@ -39,10 +39,10 @@ function Contact() {
       <div className='contactBox px-8 py-7 flex flex-col gap-8 justify-center items-start rounded-2xl shadow-md shadow-[#00ADB5]'>
         <div className='flex justify-start items-center'>
           <div className='flex flex-row justify-start items-center gap-4'>
-            <div className='text-'><IoLogoGithub className='size-8 text-[#00ADB5]'/></div>
+            <div className='text-'><IoLogoGithub className='size-8 sm:size-9 text-[#00ADB5]'/></div>
             <div className='flex flex-col justify-start items-start'>
-              <p className='font-light text-sm text-gray-400'>Github</p>
-              <p className='text-md'>github.com/mmd-rahimi</p>
+              <p className='font-light text-sm sm:text-md text-gray-400'>Github</p>
+              <p className='text-md sm:text-lg'>github.com/mmd-rahimi</p>
             </div>
           </div>
         </div>
@@ -51,10 +51,10 @@ function Contact() {
       <div className='contactBox px-8 py-7 flex flex-col gap-8 justify-center items-start rounded-2xl shadow-md shadow-[#00ADB5]'>
         <div className='flex justify-start items-center'>
           <div className='flex flex-row justify-start items-center gap-4'>
-            <div className='text-'><FaLinkedin className='size-7 text-[#00ADB5]'/></div>
+            <div className='text-'><FaLinkedin className='size-7 sm:size-8 text-[#00ADB5]'/></div>
             <div className='flex flex-col justify-start items-start'>
-              <p className='font-light text-sm text-gray-400'>Linkedin</p>
-              <p className='text-md'>-</p>
+              <p className='font-light text-sm sm:text-md text-gray-400'>Linkedin</p>
+              <p className='text-md sm:text-lg'>-</p>
             </div>
           </div>
         </div>
