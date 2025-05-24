89e0e2c7dbbc92eb180b54b7ad12ad4fb2a63768
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-05-24 11:19:48 -0700

    just need linkdin and good pic

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -36,7 +36,11 @@ function Hero() {
          lg:text-2xl lg:py-6 lg:px-12 lg:font-bold lg:rounded-4xl
          "
         >
+          <a href="../public/pdf/main-Resume.pdf"
+          download="rahimi"
+          >
           <button className="cursor-pointer">Download Resume</button>
+          </a>
         </div>
       </div>
       {/* image BG */}
